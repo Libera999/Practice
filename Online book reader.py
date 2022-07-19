ce289e49d619267e51d691670167960b17db4101
Mariam Shindauletova <106089293+Libera999@users.noreply.github.com>
2022-07-19 21:48:29 +0200

    Book reader
    Update 2

--- Online book reader.py
+++ Online book reader.py
@@ -2,12 +2,22 @@ class Library:
     def __init__(self,books):
         self.books=books
 
-    def add_book(self,book): #tuple [name, content]
+    def add_book(self,book):           #tuple [name, content]
         if self.books.get(book[0])==None:
-           self.books.get(book[0],book[1])
+           self.books[book[0]]=book[1]
            return True
         else:
            return False
+
+    def remove_book(self,book_name): #remove by name
+        if self.books.get(book_name)!=None:
+            del[self.books[book_name]]
+            return True
+        else:
+            return False
+
+    def find_book(self, book_name):     #find by name, return content
+        return self.books.get(book_name)
 
 b1={
     "Course in miracles":"kgjdfkjkj",
@@ -15,5 +25,9 @@ b1={
     }
 reader1=Library(b1)
 book1=("Seven sacred weeks","kdfkjgd")
+
 reader1.add_book(book1)
-print(reader1)
+print(reader1.books)
+
+r=reader1.find_book(book1[0])
+print(r)
