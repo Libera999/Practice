237278b530204048454825a823b2afcb9115a96a
Mariam Shindauletova <106089293+Libera999@users.noreply.github.com>
2022-07-26 22:54:32 +0300

    Fibonacci numbers, recursion

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,10 +1,10 @@
-def fib(n,cache):
+def fib_recursion(n,cache):
 
     if n==0: return 0
     elif n==1: return 1
 
     if cache[n]==0:
-        cache[n]=fib(n-1,cache)+fib(n-2,cache)
+        cache[n]=fib_recursion(n-1,cache)+fib_recursion(n-2,cache)
 
     print(cache)
     return cache[n]
@@ -12,6 +12,6 @@ def fib(n,cache):
 n=int(input("Fib number:"))
 array=[0]*(n+1)            ##cache array of zeroes
 
-f=fib(n,array)
+f=fib_recursion(n,array)
 print(f)
 
