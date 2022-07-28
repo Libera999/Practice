bf70a48f7bb6b482f5c19fb1fb0ea769de91194d
Mariam Shindauletova <106089293+Libera999@users.noreply.github.com>
2022-07-28 21:57:43 +0300

    Subsets of a set (sublists of a list)
    Update 1

--- Subsets.py
+++ Subsets.py
@@ -1,24 +1,28 @@
-def Subsets(set):
+def subsets(in_set):
     
     allsubsets = []
-    max_edge = 1<<len(set)
+    max_edge = 1<<len(in_set)
 
     for i in range(0,max_edge-1):
-        subset= binary_to_set(i,set)
+        subset= binary_to_set(i,in_set)
         allsubsets.append(subset)
 
     return allsubsets
 
 
-def binary_to_set(x, set):
+def binary_to_set(iteration, input_set):
 
-    subset={}
-    index = 0
+    subset=[]
+    i = 0
+    index=iteration
 
-    for i in range:
-        if (i and 1) == 1:
-            subset.add(set.get(index))
-
-    index+=1
+    while index>0:
+        if (index and 1) == 1:
+            subset.append(input_set[i])
+        i+=1
+        index>>=1
 
     return subset
+
+set_1=[12,2]
+print(subsets(set_1))
