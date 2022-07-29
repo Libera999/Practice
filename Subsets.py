4b03e13b2b1ea23ce1307498e1e5d0da27ad3a65
Mariam Shindauletova <106089293+Libera999@users.noreply.github.com>
2022-07-29 18:34:36 +0300

    Subsets of a set - combinatorics
    Fixed an iteration mistake. 
    Finding all sublists of a list.

--- Subsets.py
+++ Subsets.py
@@ -1,9 +1,10 @@
 def subsets(in_set):
     
     allsubsets = []
-    max_edge = 1<<len(in_set)
+ 
+    max_edge = 1<<len(in_set)      ##2^n
 
-    for i in range(0,max_edge-1):
+    for i in range(0,max_edge):
         subset= binary_to_set(i,in_set)
         allsubsets.append(subset)
 
@@ -17,12 +18,15 @@ def binary_to_set(iteration, input_set):
     index=iteration
 
     while index>0:
-        if (index and 1) == 1:
+        if (index & 1) == 1:
             subset.append(input_set[i])
         i+=1
         index>>=1
 
     return subset
 
-set_1=[12,2]
+set_1=[12,2,3,1]
 print(subsets(set_1))
+
+##b1=8
+##print(b1&1)
