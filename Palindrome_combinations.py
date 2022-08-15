6cdf6cf5162ec1d1a82032d3983c7af6b57cc78c
Mariam Shindauletova <106089293+Libera999@users.noreply.github.com>
2022-08-15 20:20:53 +0300

    Palindrome combinations/ Update 1

--- Palindrome_combinations.py
+++ Palindrome_combinations.py
@@ -5,35 +5,44 @@ def pali_combinations(str):
 
     list = [*str]
     mir = []
-
+    palindromes = []  # final set of palindromes
     center = False
 
     if len(str) == 1:  # only 1 element
         return str
 
     for i in list:
-        print(i)
+        # print(i)
+
         if list.count(i) % 2 == 0:
             mir.append(i)
             list.remove(i)
+            list.remove(i)
 
-        elif list.count(i) == 1 and len(str) % 2 != 0 and center == False:
-            mir[len(list)//2] = i
+        elif list.count(i) == 1 and len(str) % 2 != 0 and not center:
+            c_elem = i  # preserve central element
             center = True
-        # middle element
+
         else:
             return False  # no polidrome options
 
-    m = (len(list)-1)//2  # half of a list
-    half = mir[0:m]
+    # print(mir)
 
-    if m == 1 or m == 0:  # 2 or 3 elements in a string
-        return "".join(mir)
+    all_lists = permutations(mir)
 
-    elif center == False:  # even length
-        all_lists = permutations(m)
-        print(all_lists)
+    for p in all_lists:
+
+        p_l = [*p]
+        if center:
+            p_l.append(c_elem)
+
+        inverted = [p_l[len(p_l)-1-i] for i in range(len(p_l))]
+
+        s = "".join(p_l+inverted)
+        palindromes.append(s)
+
+    return palindromes
 
 
 s = input("Input the string: ")
-pali_combinations(s)
+print(pali_combinations(s))
