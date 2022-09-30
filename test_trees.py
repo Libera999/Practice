b72d8f146afb57776edca6703726d6bc30bf10cb
Mariam Shindauletova <106089293+Libera999@users.noreply.github.com>
2022-09-30 23:51:09 +0300

    Update 1

--- test_trees.py
+++ test_trees.py
@@ -4,11 +4,18 @@ from BST_Tree import Node
 
 class TestTrees(unittest.TestCase):
 
-    def test_Insert(self):
+    def setUp():
+        self.tree1 = Node(12)
 
-        tree1 = Node(12)
-        tree1.Insert(tree1, 1)
-        tree1.Insert(tree1, 8)
+    def tearDown():
+        print('teardown')
+
+    def test_Insert(self):
+        self.tree1.Insert(self.tree1, 1)
+        self.tree1.Insert(self.tree1, 8)
+
+    def test_PrintTree(self):
+        tree1.PrintTree()
 
 
 if __name__ == '__main__':
