d9bf765ff604bf1b2fcae0e4623119f8b761e54f
Mariam Shindauletova <106089293+Libera999@users.noreply.github.com>
2022-10-01 00:26:50 +0300

    Update 2
    Unit testing for BSTree class

--- test_trees.py
+++ test_trees.py
@@ -4,18 +4,23 @@ from BST_Tree import Node
 
 class TestTrees(unittest.TestCase):
 
-    def setUp():
+    def setUp(self):
         self.tree1 = Node(12)
-
-    def tearDown():
-        print('teardown')
-
-    def test_Insert(self):
         self.tree1.Insert(self.tree1, 1)
         self.tree1.Insert(self.tree1, 8)
 
+    def tearDown(self):
+        print('teardown')
+
+    def test_Insert(self):
+        print('insert')
+        self.tree1.Insert(self.tree1, 2)
+        self.tree1.Insert(self.tree1, 0)
+        self.tree1.PrintTree()
+
     def test_PrintTree(self):
-        tree1.PrintTree()
+        print('print')
+        self.tree1.PrintTree()
 
 
 if __name__ == '__main__':
